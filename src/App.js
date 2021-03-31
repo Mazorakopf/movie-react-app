f046a46e6c60bf37fc04ef1cd0b1a6ee75307381
Uladzislau Kleshchanka <uladzislau_kleshchanka1@epam.com>
2021-03-31 11:58:48 +0300

    [Task-1] : Core concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {React.createElement('h1', null, 'List of users')}
+        <UserList />
+      </div>
+    );
+  }
 }
 
-export default App;
+class UserList extends React.PureComponent {
+  render() {
+    return (
+      <ul>
+        <User firstName="Uladzislau" lastName="Kleshchanka" />
+        <User firstName="Ivan" lastName="Ivanov" />
+        <User firstName="Peter" lastName="Petrov" />
+      </ul>
+    );
+  }
+}
+
+const User = ({ firstName, lastName }) => <li>{`${firstName} ${lastName}`}</li>;
